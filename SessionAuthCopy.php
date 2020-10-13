cc6ae3ef4b93bba324eb0b0f2dca02b25edb8084
neoan <stefan.roehrl@gmail.com>
2020-10-13 13:06:54 -0400

    Prepare for neoan3 v3

--- SessionAuthCopy.php
+++ SessionAuthCopy.php
@@ -7,7 +7,8 @@ namespace Neoan3\Apps;
 interface SessionAuthCopy
 {
     public function setSecret(string $string): void;
-    public function validate(?string $jwt):SessionAuthObjectCopy;
-    public function restrict($scope):SessionAuthObjectCopy;
+    public function validate(?string $jwt = null):SessionAuthObjectCopy;
+    public function restrict($scope = []):SessionAuthObjectCopy;
     public function assign($id, $scope, $payload = []):SessionAuthObjectCopy;
+    public function logout(): bool;
 }
